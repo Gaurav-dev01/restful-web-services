c1a0468c90f315599a58d81b20112090e4d23205
Gaurav Chaudhari <gchaudhari24@gmail.com>
2023-10-19 16:40:04 +0530

    Added MessageSource (I) and helloWorldInternationalized() method that return the message.

--- src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,5 +1,9 @@
 package com.rest.webservices.restfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,23 +11,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-	//@RequestMapping(method = RequestMethod.GET,path = "/hello-world")
-	@GetMapping(path="/hello-world")
+	private MessageSource messageSource;
+
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
+
+	// @RequestMapping(method = RequestMethod.GET,path = "/hello-world")
+	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
 		return "Hello World.!";
 	}
-	
-	@GetMapping(path="/hello-world-bean")
+
+	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World.!");
 	}
-	
+
 //	Path Parameters
 //	/hello-world/path-variable/{name}
 //	/hello-world/path-variable/Gaurav
-	@GetMapping(path="/hello-world/path-variable/{name}")
+	@GetMapping(path = "/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPatVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World.!, %s", name));
 	}
-	
+
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+
+		
+	}
+
 }
